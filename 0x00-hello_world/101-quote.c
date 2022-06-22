0d9c0403d8629e0a1ec6cb08410394bd4e1a5e32
Jeremy <johnjerry012@gmail.com>
2022-06-22 10:42:52 +0000

    added 101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+/**
+ * main - Prints sizes of various types on the computer compiled and run on
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
