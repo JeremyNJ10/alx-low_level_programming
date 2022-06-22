eddcd28c92a3355a3c9d8eebbd82822bc15352be
Jeremy <johnjerry012@gmail.com>
2022-06-22 10:17:04 +0000

    fixed

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+/**
+ *  main - Prints ""Programming is like building a multilingual puzzle"
+ * 
+ *  Return: 0
+ */
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
