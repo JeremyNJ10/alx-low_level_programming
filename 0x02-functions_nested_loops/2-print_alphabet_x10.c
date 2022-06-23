7fa4d8414a679c395d12acc97b53ed7b3c5721bf
Jeremy <johnjerry012@gmail.com>
2022-06-23 06:05:54 +0000

    2-print_alphabet_x10.c file

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print lowercase alphabet ten times
+ * Return: Always 0 (success)
+ */
+
+void print_alphabet_x10(void)
+{
+	char letter;
+	int counter;
+
+	for (counter = 1; counter <= 10; counter++)
+	{
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
+		{
+			_putchar(alpha);
+		}
+		_putchar('\n');
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -21,6 +21,11 @@ void print_alphabet(void);
  * print_alphabet_x10 - print lowercase alphabet ten times
  */
 
-
+void print_alphabet_x10(void);
+/**
+ * _islower - checks for lowercase
+ * @c: the character to check
+ * Return: int
+ */
 
 #endif
