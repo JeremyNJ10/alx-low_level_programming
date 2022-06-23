b9dcc61143239565b8d4d213e014804d79ca713f
Jeremy <johnjerry012@gmail.com>
2022-06-23 08:02:51 +0000

    5-sign.c file

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -43,5 +43,11 @@ int _isalpha(int c);
  * Return: 1 if greater than 0, 0 if 0, -1 if less than 0
  */
 
+int print_sign(int n);
+/**
+ * _abs - computer absolute value of interger
+ * @int: integer
+ * Return: absolute value
+ */
 
 #endif
