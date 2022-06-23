92d2853308c73a71e26be6aa8b75d17b7d9f3c2e
Jeremy <johnjerry012@gmail.com>
2022-06-23 09:02:28 +0000

    7-print_last_digit.c files

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * print_last_digit - prints last digit
  * @n: number to compute last digit for
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -57,17 +57,4 @@ int _abs(int);
  * Return: last digit of number
  */
 
-void print_last_digit(init);
-/**
- * jack_bauer - print every minute of a day
- * Description: print minutes 00:00 to 23:59
- */
-
-void jack_bauer(void);
-/**
- * times_table - print multiplication table
- */
-
-
-
 #endif
