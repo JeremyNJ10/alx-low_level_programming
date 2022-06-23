70f04be75b91557e44c4c975b83518dc1ea000b3
Jeremy <johnjerry012@gmail.com>
2022-06-23 07:24:10 +0000

    4-isalpha.c file

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -36,4 +36,12 @@ int _islower(int c);
  * Return: 1 if lower or uppercase, 0 if not alphabet character
  */
 
+int _isalpha(int c);
+/**
+ * print_sign - print sign of number
+ * @n: number to check
+ * Return: 1 if greater than 0, 0 if 0, -1 if less than 0
+ */
+
+
 #endif
