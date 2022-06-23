43ec18ce8a94902e11c047fdbf7c6decc2a225a7
Jeremy <johnjerry012@gmail.com>
2022-06-23 21:42:39 +0000

    completed

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,53 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * _putchar - puts character to standard output
+ * @c: character to put to standard output
+ */
+
+int _putchar(char c);
+
+/**
+ * _isupper - checks for uppercase character
+ * @c: character to be checked
+ * Return: 1 if true, 0 if false
+ */
+
+int _isupper(int c);
+
+/**
+ * _isdigit - checks for a digit 0 through 9
+ * @c: digit to be checked
+ * Return: 1 if true, 0 if false
+ */
+
+int _isdigit(int c);
+
+/**
+ * mul - multiplies two integers
+ * @a: one integer
+ * @b: second integer
+ * Return: product of a and b
+ */
+
+int mul(int a, int b);
+
+/**
+ * print_numbers - print 0 to 9
+ */
+
+void print_numbers(void);
+/**
+ * print_most_numbers - print 0 to 9, skip 2 and 4
+ */
+
+void print_most_numbers(void);
+
+/**
+ * more_numbers - print 0 to 14 ten times
+ */
+
+
+
+#endif
