8aeddcd1533b54941733c272769c2a27115dcfb4
Jeremy <johnjerry012@gmail.com>
2022-06-23 05:24:41 +0000

    0-putchar.c file

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,22 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and error is set appropriately.
+ */
+
+int _putchar(char c);
+
+/**
+ * print_alphabet - print lowercase alphabet
+ */
+
+void print_alphabet(void);
+
+/**
+ * print_alphabet_x10 - print lowercase alphabet ten times
+ */
