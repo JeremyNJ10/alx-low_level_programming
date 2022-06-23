3cdeed50a6b84dd5cd2bd03cba72ae1dd9a78d95
Jeremy <johnjerry012@gmail.com>
2022-06-23 05:54:16 +0000

    1-alphabet.c file

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints lowercase alphabet
+ * Return: Always 0 (success)
+ */
+
+void print_alphabet(void)
+{
+	char letter;
+
+	alpha = 'a';
+
+	while (alpha <= 'z')
+	{
+		_putchar(alpha);
+		alpha++;
+	}
+	_putchar('\n');
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -21,4 +21,6 @@ void print_alphabet(void);
  * print_alphabet_x10 - print lowercase alphabet ten times
  */
 
+
+
 #endif
