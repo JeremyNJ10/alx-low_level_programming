ce71a06749644e4d18ded2e0b4db44dde7a58d45
Jeremy <johnjerry012@gmail.com>
2022-06-25 04:22:18 +0000

    fix

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -22,7 +22,7 @@ int wildcmp(char *s1, char *s2)
 					{
 						return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
 						{
-							else (*s1 == *s2)
+							if  (*s1 == *s2)
 							{
 								return (wildcmp(s1 + 1, s2 + 1));
 							}
