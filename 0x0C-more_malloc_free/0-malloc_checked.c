08fe7a3afe74f9fcef90d9823cdebb3cd0f155a1
Jeremy <johnjerry012@gmail.com>
2022-07-28 20:30:58 +0000

    0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+/**
+ * malloc_checked - allocate dynamic memory using malloc
+ * @b: the number of bytes to allocate
+ *
+ * Description: If memory allocation fails, cause normal process termination
+ * with a status value of 98.
+ *
+ * Return: Upon success, return a pointer to the newly allocated memory.
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *p = malloc(b);
+
+	if (p)
+		return (p);
+	exit(98)
+}
