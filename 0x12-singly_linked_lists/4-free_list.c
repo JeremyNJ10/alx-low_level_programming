112659f29b5dde22609f80863be73bd07baa1ac6
Jeremy <johnjerry012@gmail.com>
2022-08-12 00:20:03 +0000

    completed

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * free_list - a linked list
+ * @head: the first list node
+ */
+
+void free_list(list_t *head)
+{
+	if (!head)
+		return;
+
+	free_list(head->next);
+	free(head->str);
+	free(head);
+}
